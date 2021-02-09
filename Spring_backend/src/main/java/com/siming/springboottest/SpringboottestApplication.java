d356dc29941badfe7ed1d8eaeb08f7dfc06d07a5
gsm988aa <gsm988aa@gmail.com>
2021-02-09 09:20:26 +0100

    add Springboot msSQL

---
+++ Spring_backend/src/main/java/com/siming/springboottest/SpringboottestApplication.java
@@ -0,0 +1,13 @@
+package com.siming.springboottest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringboottestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringboottestApplication.class, args);
+    }
+
+}
